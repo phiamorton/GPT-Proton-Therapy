71444f6022183f14fa24be001d9b5ce988d73b3e
Phia Morton <phia@dhcp-visitor-enr-114-243.slac.stanford.edu>
2025-04-10 10:30:58 -0700

    y axis is real # of particles in energy histogram

--- plots_automated.m
+++ plots_automated.m
@@ -1,5 +1,5 @@
 % Read the data from the text file using readtable
-phioffsets = [2.65]; %[0  0.33        0.66        0.99        1.32        1.65        1.98        2.31        2.65        2.98        3.31        3.64        3.97         4.3        4.63        4.96        5.29        5.62        5.95        6.28]; %3.4; %in rad, 0-2pi
+phioffsets = [0.00]; %[0  0.33        0.66        0.99        1.32        1.65        1.98        2.31        2.65        2.98        3.31        3.64        3.97         4.3        4.63        4.96        5.29        5.62        5.95        6.28]; %3.4; %in rad, 0-2pi
 
 %plotbrowser
 
@@ -10,11 +10,22 @@ for pp = 1:length(phioffsets)
     %% Extract the columns from the table
     G = data.G;
     E=938.272*(G-1); %MeV
+
+    %translate simulated particles into real # of particles 
+    Qtot0 = 4.2e-15; %from phia_test_Emod_spreadBragg.m, going to assume this is in C
+    Qproton =1.6e-19; %C
+    sim_particles_scaling= Qtot0/Qproton
+    
+
     %% Create a plot
     figure('WindowStyle','docked', 'Name', sprintf('Energy Spectrum at Phase %.2f', phase), 'NumberTitle', 'off')
     histogram(E, 100)
     xlabel('Energy [MeV]');
     ylabel('Simulated Particles')
+
+    yyaxis right 
+    ylabel('Particles')
+    ylim([0, max(E)*sim_particles_scaling])
     title(sprintf('phase offset is %.2f radians', phase));
     % % grid on;
 end
