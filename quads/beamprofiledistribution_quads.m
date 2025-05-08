d910ecdc46a39e0baf2783d347cdcb974b3beb97
Phia Morton <phia@Phias-MacBook-Pro.local>
2025-05-08 09:22:15 -0700

    plotting to find focal point

--- quads/beamprofiledistribution_quads.m
+++ quads/beamprofiledistribution_quads.m
@@ -4,6 +4,16 @@ energyspreadpercent= 0.03;
 energy0=228.5; %MeV
 uniform=false;
 a = .005; %0.5 cm
+zlen0= 3*c/freq*beta0;  %in m
+zposE0 = zlen0/1.8; %.104
+quadpos=zposE0;
+zinit=zpos-0.1;
+tinit=(zinit)/beta0/c;
+zfin=zpos+0.6;
+tfin=(zfin)/beta0/c;
+gamma0 = (energy0+938.27)/938.27; % 1.2435;
+c = 2.998e8; %m/s
+beta0= sqrt(1-1/(gamma0^2));
 if uniform ==true
     masterfilename = sprintf('output_EnergyMod_phi%.2f_E%.2f_Esp%.2f_uniform_quads', phioffsetE, energy0, energyspreadpercent);
 else
@@ -17,17 +27,20 @@ if NoRF==true
 end
 
 simavg = readtable(sprintf('avgfull_%s.txt',masterfilename));
-avg = table2struct(simavg,'ToScalar',true)
+avg = table2struct(simavg,'ToScalar',true);
 times=avg.time;
 stdx=avg.stdx;
 stdy=avg.stdy;
 avgz=avg.avgz;
 
 figure
-scatter(avgz,stdx, 'Color', "#0072BD", 'DisplayName', 'x')
+scatter(avgz,stdx, 'Color', "#0072BD", 'DisplayName', 'average x')
 hold on
-scatter(avgz,stdy, 'Color', "red", 'DisplayName', 'y')
+scatter(avgz,stdy, 'Color', "red", 'DisplayName', 'average y')
+xline(quadpos,'--','DisplayName', 'quad position', 'LineWidth',2)
 legend();
+xlabel('Average Z [m]');
+ylabel('Transverse Profile [m]');
 % 
 % G = data.G;
 % E=938.272*(G-1); %MeV
